839144b6961debd525200d3e726b1df0db4022a8
Garry <yinfo@mail.ru>
2019-10-20 18:15:46 +0300

    Updates

---
+++ precache-manifest.e839ebb1b65f7fe0259416c5d017daaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "f3e94f1b8acb59cbfd1a",
+    "url": "/test/static/js/main.f3e94f1b.chunk.js"
+  },
+  {
+    "revision": "1068847dc6e27d73c30a",
+    "url": "/test/static/js/1.1068847d.chunk.js"
+  },
+  {
+    "revision": "f3e94f1b8acb59cbfd1a",
+    "url": "/test/static/css/main.8ca6485d.chunk.css"
+  },
+  {
+    "revision": "21839b25e4b863c09efc1cd45dcaad36",
+    "url": "/test/index.html"
+  }
+];
